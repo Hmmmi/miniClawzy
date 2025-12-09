873b16e78f16a284dafd17c275658f4f28244786
Hmmmi <593113802@qq.com>
2025-12-09 17:11:48 +0800

    Getting Started.
    1. add pkg(s): initializing.
    2. add node(s): init_dev(to initialize all pwm servos).

---
+++ src/initialzing/initialzing/init_dev.py
@@ -0,0 +1,42 @@
+"""
+@作者: Jing Mi
+@说明: 初始化设备
+"""
+
+import rclpy
+from rclpy.node import Node
+import time
+
+from .ros_control.ros_robot_controller_sdk import Board
+
+# pwm舵机初始位置
+pwm_start_positions = [
+    [1, 1500],
+    [3, 900],
+    [4, 2400],
+    [5, 1000],
+    [6, 1500]
+]
+
+"""
+初始化机械臂舵机位置
+"""
+def init_pwm_positions():
+    board = Board()
+    for position in reversed(pwm_start_positions):
+        board.pwm_servo_set_position(2, [position])
+        print(f'init pwm {position}')
+        time.sleep(2)
+
+def main(args=None):                                    # ROS2节点主入口 main函数
+    rclpy.init(args=args)                               # ROS2 Python接口初始化
+    node = Node("init_dev")                             # 创建Node对象并进行初始化
+    
+    if rclpy.ok():
+        init_pwm_positions()
+        node.get_logger().info('Initializing Over!!')
+
+    # rclpy.spin_once(node)
+    # rclpy.spin(node)                                    # 循环等待ROS2退出
+    node.destroy_node()                                 # 销毁Node对象
+    rclpy.shutdown()                                    # 关闭ROS2 Python接口
---
+++ src/initialzing/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'initialzing'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jing',
+    maintainer_email='593113802@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'init_dev = initialzing.init_dev:main',
+        ],
+    },
+)
